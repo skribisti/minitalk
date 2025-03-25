519ea218360639c351063f90c6225bc2a8d29b8f
skribisti <nonorabino1211@gmail.com>
2025-03-25 11:05:01 +0100

    makefile check

--- src/server.c
+++ src/server.c
@@ -6,25 +6,59 @@
 /*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:39:44 by norabino          #+#    #+#             */
-/*   Updated: 2025/03/13 14:13:11 by norabino         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:24:00 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+char	*ft_strjoin_char(char const *s1, char const c)
+{
+	char	*res;
+	size_t	i;
+
+	if (!c)
+		return (NULL);
+	i = 0;
+	res = (char *)malloc((ft_strlen(s1) + 2) * sizeof(char));
+	if (!res)
+		return (NULL);
+	if (s1)
+	{
+		while (s1[i])
+		{
+			res[i] = s1[i];
+			i++;
+		}
+	}
+	res[i] = c;
+	res[i + 1] = '\0';
+	if (s1)
+		free((char *)s1);
+	return (res);
+}
+
 void	handler(int signal)
 {
 	static int	bit;
-	static int	i;
+	static char	c;
+	static char	*str;
 
 	if (signal == SIGUSR1)
-		i |= 1 << bit;
+		c |= 1 << bit;
 	bit++;
 	if (bit == 8)
 	{
-		ft_printf("%c", i);
+		if (c == '\0')
+		{
+			ft_printf("%s\n", str);
+			free(str);
+			str = NULL;
+		}
+		else
+			str = ft_strjoin_char(str, (char)c);
 		bit = 0;
-		i = 0;
+		c = 0;
 	}
 }
 
